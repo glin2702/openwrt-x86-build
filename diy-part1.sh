0c3e5ba89f7503a690f7560ffb0d39d6925d7e6e
glin2702 <61492401+glin2702@users.noreply.github.com>
2026-02-20 10:53:26 +0800

    Add plugin sources and replace argon theme
    Add plugin sources and replace argon theme.

---
+++ diy-part1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 添加插件源
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+
+# 替换 argon 主题
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
