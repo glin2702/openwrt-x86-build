5003dd176cbe4b74842258c258da62d8d84690c8
glin2702 <61492401+glin2702@users.noreply.github.com>
2026-02-20 23:03:00 +0800

    Update feed-setup.sh

--- scripts/feed-setup.sh
+++ scripts/feed-setup.sh
@@ -7,4 +7,7 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 # 添加 TurboACC 源
 echo "src-git turboacc https://github.com/chenmozhijin/luci-app-turboacc.git" >> feeds.conf.default
 
+# 添加 small-package 源（包含 PassWall 依赖）
+echo "src-git small8 https://github.com/kenzok8/small-package.git;main" >> feeds.conf.default
+
 echo "软件源配置完成"
