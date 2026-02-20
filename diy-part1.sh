8a6dde4c34d4e0ab3ee195e70d20e090ad0e528b
glin2702 <61492401+glin2702@users.noreply.github.com>
2026-02-20 12:12:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# 添加插件源 - 修正后移除了无效的mv命令
+# 只添加核心需要的插件源，减少无关插件的报错风险
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 替换 argon 主题
+# 手动克隆argon主题（不依赖small8源）
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+
+# 手动克隆small源中PassWall需要的依赖（避免引入整个small8的冗余插件）
+git clone https://github.com/kenzok8/small.git package/small
